f5cf7bb312d529de743b999b9184ccbbd351fcb0
Vlad <hontarvladie@gmail.com>
2023-06-06 00:41:19 +0100

    Small note and play with Strings

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -3,6 +3,18 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
+  // 586
+
+  // UTF-8      1...4 bytes per symbol
+  // 00000010 01001010
+
+  // Byte order mark
+  // UTF-16-LE | UTF-16-BE
+  // Little endian [FFFE] | Big endian [FEFF]
+  // 00000010 01001010 | 01001010 00000010
+
+  // UTF-32
+  // 00000000 00000000 00000010 01001010
   @Test
   void string() {
 //    String str = null;
