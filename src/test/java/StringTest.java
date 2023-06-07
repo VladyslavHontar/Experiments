edc135d785829c88715c7d5139e85457e3341aff
Vlad <hontarvladie@gmail.com>
2023-06-07 21:46:55 +0100

    Playing with String methods and encodings

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -3,6 +3,9 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
+
+  static String staticField = "literal";
+
   // 586
 
   // UTF-8      1...4 bytes per symbol
@@ -32,5 +35,16 @@ public class StringTest {
     int first = repeatableString.indexOf("abc");        // use lastIndexOf to find last
     int result = repeatableString.indexOf("abc", first + 1);
     assertThat(result).isEqualTo(9);
+  }
+
+  @Test
+  void trimAndStripTest() {
+    assertThat("       \t\r\n".trim()).isEmpty();
+    assertThat("       \t\r\n".strip()).isEmpty();
+
+    assertThat("   \u0000  ab  \t\r\n".trim()).isEqualTo("ab");
+    assertThat("   \u0000  ab  \t\r\n".strip()).isEqualTo("\u0000  ab");
+    assertThat("   \u0000  ab  \t\r\n".stripLeading()).isEqualTo("\u0000  ab  \t\r\n");
+    assertThat("   \u0000  ab  \t\r\n".stripTrailing()).isEqualTo("   \u0000  ab");
   }
 }
