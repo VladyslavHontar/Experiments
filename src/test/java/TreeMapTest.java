6b6cc8f78a40aeb8cfbcee07fdf4aba3ed0b8546
Vlad <hontarvladie@gmail.com>
2023-06-23 23:04:43 +0100

    TreeMapTest polls

--- src/test/java/TreeMapTest.java
+++ src/test/java/TreeMapTest.java
@@ -1,16 +1,24 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class TreeMapTest {
   @Test
   void name() {
-    Map<String, Integer> map = new TreeMap<>();   // key cannot be null. Can be used Comparator.reverseOrder()
+    TreeMap<String, Integer> map = new TreeMap<>(Comparator.reverseOrder());   // key cannot be null. Can be used Comparator.reverseOrder()
     map.put("a", 1);
     map.put("b", 2);
     map.put(".", 3);
     map.put("_", 3);
     System.out.println(map);
+
+    map.pollFirstEntry();
+    map.pollLastEntry();
+    map.descendingKeySet();
+
+    System.out.println(map);
+
   }
 }
