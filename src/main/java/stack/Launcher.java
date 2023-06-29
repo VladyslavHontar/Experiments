7e5e579b392d6422421fe70737dac82e7533d5f8
Vlad <hontarvladie@gmail.com>
2023-06-29 16:44:45 +0100

    leetcode

--- src/main/java/stack/Launcher.java
+++ src/main/java/stack/Launcher.java
@@ -2,6 +2,7 @@ package stack;
 
 import java.util.Scanner;
 import java.util.Stack;
+import
 
 public class Launcher {
     public static void main(String[] args) {
@@ -17,6 +18,33 @@ public class Launcher {
     }
 }
 class Solution {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode list3 = new ListNode();
+        ListNode list4 = list3;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                list3.next = list1;
+                list1 = list1.next;
+            } else {
+                list3.next = list2;
+                list2 = list2.next;
+            }
+            list3 = list3.next;
+        }
+        if (list1 != null) {
+            list3.next = list1;
+        } else {
+            list3.next = list2;
+        }
+        return list4.next;
+    }
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         for(char ch: s.toCharArray()) {
