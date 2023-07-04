b4640443264230abf36ff700780c7ee285f3d9cc
Vlad <hontarvladie@gmail.com>
2023-07-04 15:12:15 +0100

    laucher

--- src/main/java/stack/Launcher.java
+++ src/main/java/stack/Launcher.java
@@ -1,11 +1,23 @@
 package stack;
 
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Launcher {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        Stack stack = new Stack();
+        while (true) {
+            String command = sc.nextLine();
+            String[] parts = command.split(" ");
+            switch (parts[0].toUpperCase()) {
+                case "PUSH" -> stack.push(command.substring(5));
+                case "POP" -> System.out.println(stack.pop());
+                case "SIZE" -> System.out.println(stack.size());
+                case "CONTAINS" -> System.out.println(stack.contains(parts[1]));
+                case "EXIT" -> {
+                    return;
+                }
+            }
+        }
     }
 }
