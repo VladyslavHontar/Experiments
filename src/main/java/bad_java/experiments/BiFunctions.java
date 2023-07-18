2a0dc44d317613c36ec987f0c561442669b9adf0
Vlad <hontarvladie@gmail.com>
2023-07-18 23:30:44 +0100

    BiFunctions

--- src/main/java/bad_java/experiments/BiFunctions.java
+++ src/main/java/bad_java/experiments/BiFunctions.java
@@ -5,8 +5,10 @@ import java.util.function.BiFunction;
 public class BiFunctions {
   public static void main(String[] args) {
     // First-class citizen (обьекты первого класса)
+    // Side effects (побочные эффекты)
     // Pure functions (чистые функции)
     // Higher-order functions (функции высшего порядка)
+    // N-arity functions (функции n-арности)
 
     BiFunction<Integer, Integer, Integer> adder = BiFunctions::add;
     int result = adder.apply(1, 2);
