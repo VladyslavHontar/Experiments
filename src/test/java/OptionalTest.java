342fb8899ffdb1fec5144b5cc5115084c236212e
Vlad <hontarvladie@gmail.com>
2023-07-23 22:03:07 +0100

    OptionalTest

--- src/test/java/OptionalTest.java
+++ src/test/java/OptionalTest.java
@@ -1,4 +1,5 @@
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -9,26 +10,45 @@ import java.util.Optional;
 public class OptionalTest {
   @Test
   void name() {
-    Optional<Client> optionalClient = getUserByName("Ivan", "Ivanov");
-    optionalClient.orElseThrow(() -> new IllegalArgumentException("User not found"));
+    Optional<Client> optionalClientIvan = getUserByName("Ivan", "Ivanov");
+    System.out.println(optionalClientIvan.filter(client -> client.getPerson().getAge() > 20)
+                                         .flatMap(Client::getLicense)
+                                         .map(License::getId)
+                                         .orElse("No license"));
+    Optional<Client> optionalClientPetr = getUserByName("Petr", "Petrov");
+    System.out.println(optionalClientPetr.flatMap(Client::getLicense)
+                                         .map(License::getId)
+                                         .orElse("No license"));
 
+    // map = T -> R
+    // flatMap = T -> Optional<R>
   }
   private static Optional<Client> getUserByName(String name, String surname) {
     if ("Ivan".equals(name) && "Ivanov".equals(surname)) {
-      Person ivan = new Person("Ivan", "Ivanov", 35);
+      Person ivan = new Person(name, surname, 18);
       LocalDate localDate = LocalDate.now();
       License license = new License(localDate, localDate.plusMonths(6), "111", new ArrayList<>(List.of(ivan)));
       return Optional.of(new Client(ivan, license));
+    }
+    if ("Petr".equals(name) && "Petrov".equals(surname)) {
+      Person petr = new Person(name, surname, 30);
+      return Optional.of(new Client(petr, null));
     }
     return Optional.empty();
   }
 
 }
+@Getter
 @AllArgsConstructor
 class Client {
   Person person;
   License license;
+
+  Optional<License> getLicense() {
+    return Optional.ofNullable(license);
+  }
 }
+@Getter
 @AllArgsConstructor
 class License {
   LocalDate registeredDate;
